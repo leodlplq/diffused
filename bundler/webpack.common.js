655ade4631adaebd15f0332f7585c9370a12b830
Léo Delplanque <delplanqueleomail@gmail.com>
2022-12-20 13:31:12 +0900

    🚑️ fix webpack

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,22 +1,22 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, "../src/script.js"),
+  entry: path.resolve(__dirname, '../src/script.js'),
   output: {
-    hashFunction: "xxhash64",
-    filename: "bundle.[contenthash].js",
-    path: path.resolve(__dirname, "../dist"),
+    hashFunction: 'xxhash64',
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   plugins: [
-    // new CopyWebpackPlugin({
-    //   patterns: [{ from: path.resolve(__dirname, "../static") }],
-    // }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, '../static') }],
+    }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "../src/index.html"),
+      template: path.resolve(__dirname, '../src/index.html'),
       minify: true,
     }),
     new MiniCSSExtractPlugin(),
@@ -26,39 +26,39 @@ module.exports = {
       // HTML
       {
         test: /\.(html)$/,
-        use: ["html-loader"],
+        use: ['html-loader'],
       },
 
       // JS
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
 
       // CSS
       {
         test: /\.css$/,
-        use: [MiniCSSExtractPlugin.loader, "css-loader"],
+        use: [MiniCSSExtractPlugin.loader, 'css-loader'],
       },
 
       // Images
       {
         test: /\.(jpg|png|gif|svg)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/images/[hash][ext]",
+          filename: 'assets/images/[hash][ext]',
         },
       },
 
       // Fonts
       {
         test: /\.(ttf|eot|woff|woff2)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "assets/fonts/[hash][ext]",
+          filename: 'assets/fonts/[hash][ext]',
         },
       },
     ],
   },
-};
+}
